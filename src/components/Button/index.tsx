6959e938f8382f9d68a42e192df88d3b112cf1c0
TiagoM13 <thyagomotha2000@gmail.com>
2023-07-03 21:49:50 -0300

    feat: add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+
+import { styles } from './styles';
+
+type Props = TouchableOpacityProps & {
+  title: string
+}
+
+export const Button = ({ title, ...rest }: Props) => {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.7} {...rest}>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
