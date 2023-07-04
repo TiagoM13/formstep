53456545cfe963d976478f680cec8e93cb6e0eef
TiagoM13 <thyagomotha2000@gmail.com>
2023-07-03 21:31:05 -0300

    feat: add form controller

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import { TextInput, View } from 'react-native';
-import { Feather } from '@expo/vector-icons'
+import React, { forwardRef } from 'react';
+import { TextInput, TextInputProps, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { Controller, UseControllerProps } from 'react-hook-form';
 
 import { styles } from './styles';
 
 type Props = {
-  icon: keyof typeof Feather.glyphMap
+  icon: keyof typeof Feather.glyphMap;
+  formProps: UseControllerProps;
+  inputProps: TextInputProps;
 }
 
-export const Input = ({ icon }: Props) => {
+export const Input = forwardRef<TextInput, Props>(({ icon, formProps, inputProps }, ref) => {
   return (
-    <View style={styles.group}>
-      <View style={styles.icon}>
-        <Feather name={icon} size={24} color='red' />
-      </View>
-      <TextInput style={styles.control} />
-    </View>
+    <Controller
+      render={() => (
+        <View style={styles.group}>
+          <View style={styles.icon}>
+            <Feather name={icon} size={24} color='red' />
+          </View>
+
+          <TextInput
+            ref={ref}
+            style={styles.control}
+            {...inputProps}
+          />
+        </View>
+      )}
+      {...formProps}
+    />
   );
-}
+})
